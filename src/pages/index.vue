64afc8dc5723782b1cff3445bd2f1b75fd916688
alfarezyyd <adityaalfarezyd@gmail.com>
2025-12-08 16:02:47 +0700

    feat(dashboard): Re-layout dashboard

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,18 +23,75 @@ import HourlyEnergyConsumptionTrendDiagramChart
     </VRow>
     <VRow class="match-height">
       <VCol
+        cols="12"
+        md="6"
+        sm="6"
+      >
+        <AnalyticsAverageDailySales />
+      </VCol>
+      <VCol
+        cols="12"
+        md="6"
+        sm="6"
+      >
+        <AnalyticsAverageDailySales />
+      </VCol>
+    </VRow>
+    <VRow class="match-height">
+      <VCol
         class="d-flex"
-        cols="7"
-        lg="7"
-        md="7"
+        cols="8"
+        lg="8"
+        md="8"
       >
         <ThreeViewer class="flex-grow-1" />
       </VCol>
       <VCol
+        cols="12"
+        lg="4"
+        md="4"
+      >
+        <VRow class="match-height">
+          <VCol
+            cols="12"
+            lg="12"
+            md="12"
+          >
+            <VCard>
+              <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
+                <VCardTitle>Power (kW/hr)</VCardTitle>
+              </VCardItem>
+
+              <VCardText>
+                <PowerChart />
+              </VCardText>
+            </VCard>
+          </VCol>
+          <VCol
+            cols="12"
+            lg="12"
+            md="12"
+          >
+            <VCard>
+              <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
+                <VCardTitle>Production (kW/hr)</VCardTitle>
+              </VCardItem>
+
+              <VCardText>
+                <ProductionChart />
+              </VCardText>
+            </VCard>
+          </VCol>
+        </VRow>
+      </VCol>
+    </VRow>
+
+    <VRow class="match-height">
+      <VCol
         class="d-flex"
-        cols="5"
-        lg="5"
-        md="5"
+        cols="4"
+        lg="4"
+        md="4"
       >
         <VCard class="flex-grow-1">
           <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
@@ -63,45 +120,11 @@ import HourlyEnergyConsumptionTrendDiagramChart
           </VCardText>
         </VCard>
       </VCol>
-    </VRow>
-    <VRow class="match-height">
+
       <VCol
         cols="12"
-        lg="6"
-        md="6"
-      >
-        <VCard>
-          <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-            <VCardTitle>Power (kW/hr)</VCardTitle>
-          </VCardItem>
-
-          <VCardText>
-            <PowerChart />
-          </VCardText>
-        </VCard>
-      </VCol>
-      <VCol
-        cols="12"
-        lg="6"
-        md="6"
-      >
-        <VCard>
-          <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-            <VCardTitle>Production (kW/hr)</VCardTitle>
-          </VCardItem>
-
-          <VCardText>
-            <ProductionChart />
-          </VCardText>
-        </VCard>
-      </VCol>
-    </VRow>
-
-    <VRow class="match-height">
-      <VCol
-        cols="12"
-        lg="7"
-        md="7"
+        lg="8"
+        md="8"
       >
         <VCard class="fill-height">
           <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
@@ -110,66 +133,24 @@ import HourlyEnergyConsumptionTrendDiagramChart
           </VCardItem>
 
           <VCardText>
-            <ApexChartAreaChart />
-          </VCardText>
-        </VCard>
-      </VCol>
+            <VRow class="h-100">
+              <VCol
+                cols="12"
+                lg="8"
+                md="8"
+              >
+                <ApexChartAreaChart />
+              </VCol>
 
-      <VCol
-        cols="12"
-        lg="5"
-        md="5"
-      >
-        <!-- 👇 Bungkus semua dengan VRow -->
-        <VRow class="fill-height">
-          <VCol cols="12">
-            <EcommerceExpensesRadialBarCharts />
-          </VCol>
-          <VCol
-            cols="12"
-            md="6"
-            sm="6"
-          >
-            <AnalyticsAverageDailySales />
-          </VCol>
-          <VCol
-            cols="12"
-            md="6"
-            sm="6"
-          >
-            <AnalyticsAverageDailySales />
-          </VCol>
-        </VRow>
-      </VCol>
-    </VRow>
-    <VRow class="match-height">
-      <VCol
-        cols="12"
-        lg="6"
-        md="6"
-      >
-        <VCard>
-          <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-            <VCardTitle>Daily Energy Consumption Trend Diagram</VCardTitle>
-          </VCardItem>
-
-          <VCardText>
-            <DailyEnergyConsumptionTrendDiagramChart />
-          </VCardText>
-        </VCard>
-      </VCol>
-      <VCol
-        cols="12"
-        lg="6"
-        md="6"
-      >
-        <VCard>
-          <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-            <VCardTitle>Hourly Energy Consumption Trend Diagram</VCardTitle>
-          </VCardItem>
-
-          <VCardText>
-            <HourlyEnergyConsumptionTrendDiagramChart />
+              <VCol
+                class="d-flex align-center justify-center h-100"
+                cols="12"
+                lg="4"
+                md="4"
+              >
+                <EcommerceExpensesRadialBarCharts />
+              </VCol>
+            </VRow>
           </VCardText>
         </VCard>
       </VCol>
