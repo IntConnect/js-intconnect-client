c739e46205d1a13da2232474d2f9456ba0e07e7e
alfarezyyd <adityaalfarezyd@gmail.com>
2026-01-10 03:17:17 +0700

    feat(system-setting): Add listener setting card

--- src/pages/system-settings/index.vue
+++ src/pages/system-settings/index.vue
@@ -7,12 +7,7 @@ import UserProfileHeaderBg from '@images/pages/user-profile-header-bg.png';
 <template>
   <VCol cols="12">
     <VCard>
-      <VImg
-        :src="UserProfileHeaderBg"
-        cover
-        max-height="250"
-        min-height="125"
-      />
+      <VImg :src="UserProfileHeaderBg" cover max-height="250" min-height="125" />
 
       <VCardText class="d-flex align-bottom flex-sm-row flex-column justify-center gap-x-6">
         <div class="user-profile-info w-100 mt-16 pt-6 pt-sm-0 mt-sm-0">
@@ -23,20 +18,15 @@ import UserProfileHeaderBg from '@images/pages/user-profile-header-bg.png';
           <div class="d-flex align-center justify-center justify-sm-space-between flex-wrap gap-5">
             <div class="d-flex flex-wrap justify-center justify-sm-start flex-grow-1 gap-6">
               <span class="d-flex gap-x-2 align-center">
-                <VIcon
-                  icon="tabler-map-pin"
-                  size="24"
-                />
+                <VIcon icon="tabler-map-pin" size="24" />
                 <div class="text-body-1 font-weight-medium">
-                  Jl. Raya Kawasan Industri Indotaisei, Sektor 1A, Blok Q1, Kalihurip, Cikampek, Karawang, West Java 41373
+                  Jl. Raya Kawasan Industri Indotaisei, Sektor 1A, Blok Q1, Kalihurip, Cikampek, Karawang, West Java
+                  41373
                 </div>
               </span>
 
               <span class="d-flex gap-x-2 align-center">
-                <VIcon
-                  icon="tabler-calendar"
-                  size="24"
-                />
+                <VIcon icon="tabler-calendar" size="24" />
                 <div class="text-body-1 font-weight-medium">
                   Development Initiated in 20 November 2025
                 </div>
@@ -48,65 +38,49 @@ import UserProfileHeaderBg from '@images/pages/user-profile-header-bg.png';
     </VCard>
   </VCol>
   <VRow class="d-flex flex-row match-height mb-5">
-  <VCol
-    cols="12"
-    lg="4"
-    md="4"
-  >
-    <VCard class="mt-5">
-      <VImg
-        :src="pages1"
-        cover
-        style="max-height: 250px;"
+    <VCol cols="12" lg="4" md="4">
+      <VCard class="mt-5">
+        <VImg :src="pages1" cover style="max-height: 250px;" />
 
-      />
+        <VCardItem>
+          <VCardTitle class="d-flex justify-space-between">
+            <span>Dashboard Settings</span>
 
-      <VCardItem>
-        <VCardTitle>Dashboard Settings</VCardTitle>
-      </VCardItem>
+            <VBtn variant="tonal" to="system-settings/dashboard-settings">
+              Manage
+            </VBtn>
+          </VCardTitle>
+        </VCardItem>
 
-      <VCardText>
-        Centralized control for dashboard configuration and customization.
-      </VCardText>
-      <VCardActions>
-        <RouterLink to="system-settings/dashboard-settings">
-          <VBtn variant="tonal">
-            Manage
-          </VBtn>
-        </RouterLink>
-      </VCardActions>
-    </VCard>
-    
-  </VCol>
-   <VCol
-    cols="12"
-    lg="4"
-    md="4"
-  >
-    <VCard class="mt-5">
-      <VImg
-        :src="pages2"
-        cover
-        style="max-height: 250px;"
-      />
+        <VCardText>
+          Centralized control for dashboard configuration and customization.
+        </VCardText>
 
-      <VCardItem>
-        <VCardTitle>Listener Settings</VCardTitle>
-      </VCardItem>
+      </VCard>
 
-      <VCardText>
-Configure and manage listener parameters for real-time data processing.
-      </VCardText>
-      <VCardActions>
-        <RouterLink to="system-settings/listener-settings">
-          <VBtn variant="tonal">
-            Manage
-          </VBtn>
-        </RouterLink>
-      </VCardActions>
-    </VCard>
-    
-  </VCol>
+    </VCol>
+    <VCol cols="12" lg="4" md="4">
+      <VCard class="mt-5">
+        <VImg :src="pages2" cover style="max-height: 250px;" />
+
+        <VCardItem>
+          <VCardTitle class="d-flex justify-space-between">
+            <span>Listener Settings</span>
+
+            <VBtn variant="tonal" to="system-settings/listener-settings">
+              Manage
+            </VBtn>
+          </VCardTitle>
+
+        </VCardItem>
+
+        <VCardText>
+          Configure and manage listener parameters for real-time data processing.
+        </VCardText>
+
+      </VCard>
+
+    </VCol>
   </VRow>
 </template>
 
